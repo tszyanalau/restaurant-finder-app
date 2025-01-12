c9a9c976f02daf4832838be596419953bb8ab0bc
Amy Lau <tszyanalau@gmail.com>
2025-01-13 03:15:03 +0900

    chore: add ignored files to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import pluginReact from 'eslint-plugin-react'
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { ignores: ['dist/*'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
